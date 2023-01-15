beae7878775e82771ebde9362d5793d46f9df752
BlissArmah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-01-15 22:29:22 +0000

    hacker project done

--- 22-hacker-news/setup/src/SearchForm.js
+++ 22-hacker-news/setup/src/SearchForm.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const SearchForm = () => {
-  return <h2>search form</h2>
+  const {searchStory,query} = useGlobalContext()
+  return <form className='search-form' onSubmit={(e)=> e.preventDefault()}>
+    <h2>search hacker new</h2>
+    <input type="text" className='form-input' value={query} onChange={(e)=>searchStory(e.target.value)} />
+  </form>
 }
 
 export default SearchForm
