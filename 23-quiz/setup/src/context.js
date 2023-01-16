ee217ba24b9284b1e24f74830c7fdc9370cc2695
BlissArmah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-01-16 21:42:06 +0000

    quiz api

--- 23-quiz/setup/src/context.js
+++ 23-quiz/setup/src/context.js
@@ -10,11 +10,38 @@ const table = {
 const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
 const url = ''
-
+const tempUrl ='https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+
+  const [waiting,setWaiting] = useState(true);
+  const [loading,setLoading] = useState(false);
+  const [questions,setQuestions] = useState([]);
+  const [index,setIndex] = useState(0);
+  const [correct,setCorrect] = useState(0);
+  const [error,setError] = useState(false);
+
+  const [isModalOpen,setIsModalOpen] = useState(false)
+
+const fetchQuestion =  async(url) =>{
+  setLoading(true)
+  setWaiting(false)
+  const response = await axios(url).catch(err => console.log(err))
+  if(response){
+    const data = response.data.results
+    console.log(data);
+  }
+  else{
+    setWaiting(true)
+  }
+}
+
+useEffect(()=>{
+  fetchQuestion(tempUrl)
+},[])
+
+  return <AppContext.Provider value={{waiting,loading,questions,index,correct,error,isModalOpen}}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
